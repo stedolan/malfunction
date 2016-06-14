7a65be3f2cce47d303a06b90be745f5fe15e7291
Stephen Dolan <stephen.dolan@cl.cam.ac.uk>
2016-06-14 01:38:24 +0100

    Arithmetic in the interpreter

--- src/malfunction_interpreter.ml
+++ src/malfunction_interpreter.ml
@@ -6,6 +6,120 @@ type value =
 | Func of (value -> value)
 | Int of intconst
 
+module type IntType = sig
+  type t
+  val of_int : int -> t
+  val of_string : string -> t
+
+  val neg : t -> t
+  val lognot : t -> t
+
+  val add : t -> t -> t
+  val sub : t -> t -> t
+  val mul : t -> t -> t
+  val div : t -> t -> t
+  val rem : t -> t -> t
+  val logand : t -> t -> t
+  val logor : t -> t -> t
+  val logxor : t -> t -> t
+  val shift_left : t -> int -> t
+  val shift_right_logical : t -> int -> t
+  val shift_right : t -> int -> t
+
+  val compare : t -> t -> int
+
+  val of_val : value -> t
+  val to_val : t -> value
+end
+
+module IntTypeInt = struct
+  type t = int
+  let of_int x = x
+  let of_string = int_of_string
+  let neg = (~-)
+  let lognot = (lnot)
+  let add = (+)
+  let sub = (-)
+  let mul = ( * )
+  let div = (/)
+  let rem = (mod)
+  let logand = (land)
+  let logor = (lor)
+  let logxor = (lxor)
+  let shift_left = (lsl)
+  let shift_right_logical = (lsr)
+  let shift_right = (asr)
+  let compare = compare
+
+  let of_val = function
+    | Int (`Int n) -> n
+    | _ -> failwith "expected int"
+  let to_val n = Int (`Int n)
+end
+
+module Arith (I : IntType) = struct
+  open I
+  let unary_op op a = match op with
+    | `Neg -> to_val (neg (of_val a))
+    | `Not -> to_val (lognot (of_val a))
+
+  type op_normal = [`Add|`Sub|`Mul|`Div|`Mod|`And|`Or|`Xor]
+  type op_shift = [`Lsl|`Lsr|`Asr]
+  type op_cmp = [`Lt|`Gt|`Lte|`Gte|`Eq]
+
+  let binary_op op a b = match op with
+    | #op_normal as op ->
+       let a = of_val a and b = of_val b in
+       to_val ((match op with
+       | `Add -> add | `Sub -> sub
+       | `Mul -> mul | `Div -> div | `Mod -> rem
+       | `And -> logand | `Or -> logor | `Xor -> logxor) a b)
+    | #op_shift as op ->
+       let a = of_val a and k = IntTypeInt.of_val b in
+       to_val ((match op with
+       | `Lsl -> shift_left
+       | `Lsr -> shift_right_logical
+       | `Asr -> shift_right) a k)
+    | #op_cmp as op ->
+       let a = of_val a and b = of_val b in
+       let int_of_bool x = Int (`Int (if x then 1 else 0)) in
+       let cmp = compare a b in
+       int_of_bool (match op with
+       | `Lt -> cmp < 0
+       | `Gt -> cmp > 0
+       | `Lte -> cmp <= 0
+       | `Gte -> cmp >= 0
+       | `Eq -> cmp = 0)
+end
+
+module ArithInt = Arith(IntTypeInt)
+module ArithInt32 = Arith(struct
+  include Int32
+  let of_val = function
+    | Int (`Int32 n) -> n
+    | _ -> failwith "expected int32"
+  let to_val n = Int (`Int32 n)
+end)
+module ArithInt64 = Arith(struct
+  include Int64
+  let of_val = function
+    | Int (`Int64 n) -> n
+    | _ -> failwith "expected int64"
+  let to_val n = Int (`Int64 n)
+end)
+module ArithBigint = Arith(struct
+  include Z
+  let shift_right_logical = shift_right
+  let of_val = function
+    | Int (`Bigint n) -> n
+    | _ -> failwith "expected bigint"
+  let to_val n = Int (`Bigint n)
+end)
+
+
+
+
+
 let rec interpret locals env : t -> value = function
   | Mvar v -> Ident.Map.find v locals
   | Mlambda (xs, e) ->
@@ -47,11 +161,15 @@ let rec interpret locals env : t -> value = function
      Seq (`Bytevec, `Imm, 
           Array.init (String.length s) (fun i -> Int (`Int (Char.code (String.get s i)))))
   | Mglobal v -> failwith "globals unsupported"
-(*
+     (*
      let (path, _descr) = Env.lookup_value v env in
-     let rec lookup = function
+     let path = Env.normalize_path None env path in
+     let rec lookup = let open Path in function
        | Pident id -> Symtable.get_global_value id
-*)
+       | Pdot (path, _, i) -> Obj.field (lookup path) i
+       | Papply _ -> failwith "functor application in global reference" in
+     lookup path
+     *)
   | Mswitch (scr, cases) ->
      let scr = interpret locals env scr in
      let rec find_match = function
@@ -67,23 +185,19 @@ let rec interpret locals env : t -> value = function
        | [] -> failwith "no case matches" in
      find_match cases
   | Mintop1 (op, ty, e) ->
-     let v = match interpret locals env e with
-       | Int k -> k | _ -> failwith "expected integer" in
-     Int (match ty, op, v with
-     | `Int, `Neg, `Int n -> `Int (~-n)
-     | `Int, `Not, `Int n -> `Int (lnot n)
-     | `Int32, `Neg, `Int32 n -> `Int32 (Int32.neg n)
-     | `Int32, `Not, `Int32 n -> `Int32 (Int32.lognot n)
-     | `Int64, `Neg, `Int64 n -> `Int64 (Int64.neg n)
-     | `Int64, `Not, `Int64 n -> `Int64 (Int64.lognot n)
-     | `Bigint, `Neg, `Bigint n -> `Bigint (Z.neg n)
-     | `Bigint, `Not, `Bigint n -> `Bigint (Z.lognot n)
-     | _ -> failwith "wrong integer type")
+     let fn = match ty with
+       | `Int -> ArithInt.unary_op
+       | `Int32 -> ArithInt32.unary_op
+       | `Int64 -> ArithInt64.unary_op
+       | `Bigint -> ArithBigint.unary_op in
+     fn op (interpret locals env e)
   | Mintop2 (op, ty, e1, e2) ->
-     let (v1, v2) = match interpret locals env e1, interpret locals env e2 with
-       | Int k1, Int k2 -> k1, k2
-       | _ -> failwith "expected integers" in
-     Int (failwith "unimplemented!")
+     let fn = match ty with
+       | `Int -> ArithInt.binary_op
+       | `Int32 -> ArithInt32.binary_op
+       | `Int64 -> ArithInt64.binary_op
+       | `Bigint -> ArithBigint.binary_op in
+     fn op (interpret locals env e1) (interpret locals env e2)
   | Mseqget (ty, seq, idx) ->
      (match interpret locals env seq, interpret locals env idx with
      | Seq (ty', _, vals), Int (`Int i) when ty = ty' -> vals.(i)
