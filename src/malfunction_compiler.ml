stedolan/malfunction

commit 571cd8611f24986c41c5815f13c679f347477314

Support for -package and -dontlink in linking of output files (#38)
* Support -linkpkg, -package and -dontlink in compile command

* Remove default zarith linking in compile

* optimization option

* Update malfunction.opam

Remove unnecessary version field

---------

Co-authored-by: Yannick Forster <yannick.forster@inria.fr>

src/malfunction_compiler.ml
@@ -632,11 +632,9 @@ let setup_options options =
   Clflags.inlining_report := false;
   Clflags.dlcode := true;
   Clflags.shared := false;
-  Clflags.(
-    default_simplify_rounds := 2;
-    use_inlining_arguments_set o2_arguments;
-    use_inlining_arguments_set ~round:0 o1_arguments);
+  Clflags.(default_simplify_rounds := 0);
   (* FIXME: should we use classic_arguments for non-flambda builds? *)
+
 
   (* Hack: disable the "no cmx" warning for zarith *)
   let _ = Warnings.parse_options false "-58" in
@@ -646,11 +644,11 @@ let setup_options options =
   | `Verbose ->
      Clflags.dump_lambda := true;
      Clflags.dump_cmm := true;
+     Clflags.keep_asm_file := true;
+     Clflags.inlining_report := true
      (*
        If anyone wants to keep these, there should probably be another option for where to put them.
        (rather than leaving stale temporary directories around)
-     Clflags.keep_asm_file := true;
-     Clflags.inlining_report := true
       *)
   | `Shared ->
      Clflags.shared := true
@@ -658,7 +656,16 @@ let setup_options options =
      let packages = String.split_on_char ',' s in
      let dirs = List.map Findlib.package_directory packages in
      Clflags.include_dirs := dirs @ !Clflags.include_dirs
-  | `ForPack s -> Clflags.for_package := Some s);
+  | `ForPack s -> Clflags.for_package := Some s
+  | `Dontlink _ -> ()
+  | `Linkpkg -> ()
+  | `Thread -> ()
+  | `Optimize ->   Clflags.(
+    default_simplify_rounds := 2;
+    use_inlining_arguments_set o2_arguments;
+    use_inlining_arguments_set ~round:0 o1_arguments);
+   );
+  (* FIXME: should we use classic_arguments for non-flambda builds? *)
 
   Compenv.(readenv Format.std_formatter (Before_compile "malfunction"));
   compmisc_init_path ()
@@ -752,8 +759,7 @@ let delete_temps { objfile; cmxfile; cmifile } =
   match cmifile with Some f -> Misc.remove_file f | None -> ()
 
 
-type options = [`Verbose | `Shared | `ForPack of string | `Package of string] list
-
+type options = [`Verbose | `Shared | `ForPack of string | `Package of string | `Dontlink of string | `Linkpkg | `Thread | `Optimize] list
 
 let lambda_to_cmx ?(options=[]) ~filename ~prefixname ~module_name ~module_id lmod =
   let ppf = Format.std_formatter in
@@ -868,7 +874,34 @@ let compile_and_load ?(options : options =[]) e =
 
 
 
-let link_executable output tmpfiles =
-  (* urgh *)
-  Sys.command (Printf.sprintf "ocamlfind ocamlopt -package zarith zarith.cmxa '%s' -o '%s'"
-                 tmpfiles.cmxfile output)
+let link_executable ?options output tmpfiles =
+   let options = match options with Some l -> l | None -> [] in
+   let pkgs =
+      options |> (List.filter_map @@ 
+      function
+      | `Package s -> Some s
+      | _ -> None)
+   in
+   let linkpkg = options |> (List.exists @@ (function `Linkpkg -> true | _ -> false)) in
+   let dontlink =
+      options |> (List.filter_map @@ 
+      function
+      | `Dontlink s -> Some s
+      | _ -> None)
+   in
+   let thread =
+      options |> (List.exists @@ 
+      function
+      | `Thread -> true
+      | _ -> false)
+   in
+   let pkgs = String.concat "," pkgs in
+   let dontlink = 
+      if dontlink = [] then ""
+      else "-dontlink " ^ (String.concat "," dontlink)
+   in
+   let linkpkg = if linkpkg then "-linkpkg " else "" in
+   let thread = if thread then "-thread " else "" in
+   (* urgh *)
+   Sys.command (Printf.sprintf "ocamlfind ocamlopt %s %s -package %s %s '%s' -o '%s'"
+                 thread linkpkg pkgs dontlink tmpfiles.cmxfile output)

src/malfunction_main.ml
@@ -4,8 +4,8 @@ open Malfunction
 let usage () =
   Printf.fprintf stderr "%s" @@
     "Malfunction v0.1. Usage:\n"^
-    "   malfunction compile [-v] [-o output] input.mlf\n" ^
-    "     Compile input.mlf to an executable\n\n" ^
+    "   malfunction compile [-v] [-thread] [-linkpkg] [-dontlink pack1,...,packn] [-package pack1,...packn] [-o output] input.mlf\n" ^
+    "     Compile input.mlf to an executable using ocamlfind\n" ^
     "   malfunction cmx [-v] [-shared] [-package pack1,...,packn] [-for-pack s] input.mlf\n" ^
     "     Compile input.mlf to input.cmx, for linking with ocamlopt.\n"^
     "     Package \"zarith\" is always included.\n\n" ^
@@ -43,7 +43,7 @@ let run mode options impl output =
        let output = match output with
          | None -> Compenv.output_prefix file
          | Some out -> out in
-       let res = Malfunction_compiler.link_executable output tmpfiles in
+       let res = Malfunction_compiler.link_executable ~options output tmpfiles in
        Malfunction_compiler.delete_temps tmpfiles;
        res)
   | `Eval, Some _file ->
@@ -68,6 +68,16 @@ let parse_args args =
     | "-shared" :: rest -> opts := `Shared :: !opts; parse_opts mode rest
     | "-for-pack" :: o :: rest -> opts := `ForPack o :: !opts; parse_opts mode rest
     | "-package" :: s :: rest -> opts := `Package s :: !opts; parse_opts mode rest
+    | "-dontlink" :: s :: rest -> 
+      if mode = `Compile then (opts := `Dontlink s :: !opts; parse_opts mode rest)
+      else usage ()
+    | "-linkpkg" :: rest -> 
+      if mode = `Compile then (opts := `Linkpkg :: !opts; parse_opts mode rest)
+      else usage ()
+    | "-thread" :: rest -> 
+        if mode = `Compile then (opts := `Thread :: !opts; parse_opts mode rest)
+        else usage ()
+    | "-O2" :: rest -> opts := `Optimize :: !opts; parse_opts mode rest
     | i :: rest ->
        (match !impl with None -> (impl := Some i; parse_opts mode rest) | _ -> usage ())
     | [] -> run mode !opts !impl !output in

src/malfunction_compiler.mli
@@ -6,7 +6,7 @@ type outfiles = {
 }
 val delete_temps : outfiles -> unit 
 
-type options = [`Verbose | `Shared | `ForPack of string | `Package of string] list
+type options = [`Verbose | `Shared | `ForPack of string | `Package of string | `Dontlink of string | `Linkpkg | `Thread | `Optimize] list
 
 val compile_module :
   ?options:options ->
@@ -16,6 +16,6 @@ val compile_module :
 
 val compile_cmx : ?options:options -> string -> outfiles
 
-val link_executable : string -> outfiles -> int
+val link_executable : ?options:options -> string -> outfiles -> int
 
 val compile_and_load : ?options:options -> Malfunction.t -> Obj.t

commit 2add775b984ae0666ca5220524f8259557a6fe4b

Fix two issues introduced in #38
  - 'malfunction compile' now works again with an empty package list
  - testsuite now explicitly links zarith (required for some tests)

src/malfunction_compiler.ml
@@ -895,13 +895,17 @@ let link_executable ?options output tmpfiles =
       | `Thread -> true
       | _ -> false)
    in
-   let pkgs = String.concat "," pkgs in
+   let pkgs =
+     match pkgs with
+     | [] -> []
+     | pkgs -> ["-package"; String.concat "," pkgs] in
    let dontlink = 
-      if dontlink = [] then ""
-      else "-dontlink " ^ (String.concat "," dontlink)
+      if dontlink = [] then []
+      else ["-dontlink"; String.concat "," dontlink]
    in
-   let linkpkg = if linkpkg then "-linkpkg " else "" in
-   let thread = if thread then "-thread " else "" in
+   let linkpkg = if linkpkg then ["-linkpkg"] else [] in
+   let thread = if thread then ["-thread"] else [] in
+   let opts = String.concat " " (thread @ linkpkg @ pkgs @ dontlink) in
    (* urgh *)
-   Sys.command (Printf.sprintf "ocamlfind ocamlopt %s %s -package %s %s '%s' -o '%s'"
-                 thread linkpkg pkgs dontlink tmpfiles.cmxfile output)
+   Sys.command (Printf.sprintf "ocamlfind ocamlopt %s '%s' -o '%s'"
+                 opts tmpfiles.cmxfile output)

src/malfunction_main.ml
@@ -8,7 +8,6 @@ let usage () =
     "     Compile input.mlf to an executable using ocamlfind\n" ^
     "   malfunction cmx [-v] [-shared] [-package pack1,...,packn] [-for-pack s] input.mlf\n" ^
     "     Compile input.mlf to input.cmx, for linking with ocamlopt.\n"^
-    "     Package \"zarith\" is always included.\n\n" ^
     "   malfunction eval\n" ^
     "     Run a REPL to evaluate expressions with the interpreter\n\n" ^
     "   malfunction fmt\n" ^

test/test.ml
@@ -76,11 +76,12 @@ let try_run_tests cases =
     Misc.remove_file exec_name;
     match !temps with Some t -> Malfunction_compiler.delete_temps t | None -> () in
 
+  let options = [`Package "zarith"; `Linkpkg] in
   begin match
     Mmod ([`Unnamed (Mapply (checker, [Mblock (0, testcases)]))], [])
-    |> Malfunction_compiler.compile_module ~filename:exec_name
+    |> Malfunction_compiler.compile_module ~options ~filename:exec_name
     |> (fun t -> temps := Some t; t)
-    |> Malfunction_compiler.link_executable exec_name
+    |> Malfunction_compiler.link_executable ~options exec_name
   with
   | 0 -> ()
   | _ -> delete_temps (); raise (HarnessFailed "Link error")